fc5225953ac5ee7dd34532b80a93223046189d0e
borracciaBlu <marco.asdman@gmail.com>
2017-05-16 15:04:11 +1000

    [MOD] Add tests for CopySettingFile.

---
+++ test/AwsUpload/Command/CopySettingFileTest.php
@@ -0,0 +1,91 @@
+<?php
+
+use AwsUpload\Io\Output;
+use AwsUpload\AwsUpload;
+use AwsUpload\Setting\SettingFiles;
+use AwsUpload\Facilitator;
+use Symfony\Component\Filesystem\Filesystem;
+
+require_once __DIR__ . '/../BaseTestCase.php';
+
+
+class CopySettingsFileTest extends BaseTestCase
+{
+
+    public function test_noKey_expectedNoProjectMsg()
+    {
+        $msg = Facilitator::onNoCopyArgs();
+        $msg = Output::color($msg);
+        $this->expectOutputString($msg);
+
+        self::clearArgv();
+        self::pushToArgv(array('asd.php', 'copy'));
+
+        $aws = new AwsUpload();
+        $aws->is_phpunit = true;
+
+        $cmd = new \AwsUpload\Command\CopySettingFile($aws);
+        $cmd->run();
+    }
+
+    public function test_noValidKey_expectedNoValidKey()
+    {
+        $msg = Facilitator::onNoCopyArgs();
+        $msg = Output::color($msg);
+        $this->expectOutputString($msg);
+
+        self::clearArgv();
+        self::pushToArgv(array('asd.php', 'copy', 'aaa'));
+
+        $aws = new AwsUpload();
+        $aws->is_phpunit = true;
+
+        $cmd = new \AwsUpload\Command\CopySettingFile($aws);
+        $cmd->run();
+    }
+
+    public function test_validKeyNoExists_expectedNoFileFound()
+    {   
+        $filesystem = new Filesystem();
+        $filesystem->dumpFile($this->directory . '/project-1.dev.json', '{}');
+
+        $msg = Facilitator::onNoFileFound('project-2.dev');
+        $msg = Output::color($msg);
+        $this->expectOutputString($msg);
+
+        self::clearArgv();
+        self::pushToArgv(array('asd.php', 'copy', 'project-2.dev', 'project-3.dev'));
+
+        $aws = new AwsUpload();
+        $aws->is_phpunit = true;
+
+        $cmd = new \AwsUpload\Command\CopySettingFile($aws);
+        $cmd->run();
+    }
+
+    public function test_copyFile()
+    {   
+        $filesystem = new Filesystem();
+        $filesystem->dumpFile($this->directory . '/project-1.dev.json', '{"pem": "", "local":"", "remote":"", "exclude":[""]}');
+
+
+        $msg = Facilitator::onNewSettingFileSuccess('project-2.dev');
+        $msg = Output::color($msg);
+        $this->expectOutputString($msg);
+
+        self::clearArgv();
+        self::pushToArgv(array('asd.php', 'copy', 'project-1.dev', 'project-2.dev'));
+
+
+        $aws = new AwsUpload();
+        $aws->is_phpunit = true;
+
+        $cmd = new \AwsUpload\Command\CopySettingFile($aws);
+        $cmd->run();
+       
+        $settings = SettingFiles::getObject('project-2.dev');
+
+        $sample = (object) ["pem" => "", "local" => "", "remote" => "", "exclude" => [""]];
+        $this->assertEquals($sample, $settings);
+    }
+}
